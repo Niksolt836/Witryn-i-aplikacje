43c5b4ddac66ac11ec180d4c6ee1ed48cb91c1d0
Niksolt836 <niksolt3@edu.gdansk.pl>
2025-10-05 22:14:56 +0200

    Add files via upload

--- soltysek_zewnetrzny.js
+++ soltysek_zewnetrzny.js
@@ -1,3 +1,3 @@
-document.write("ostatnia modyfikacja strony".fontcolor("maroon").bold().fontsize(7)+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("maroon").strike().bold().fontsize(7)+"<br>"); 
 
-document.write(document.lastModified); 
+document.write(document.lastModified.fontcolor("red").strike().fontsize(6)); 
