7d02f6c9876cc76541dc51a77799d574e07652b5
Niksolt836 <niksolt3@edu.gdansk.pl>
2025-10-05 22:06:45 +0200

    Update soltysek_zewnetrzny.js

--- soltysek_zewnetrzny.js
+++ soltysek_zewnetrzny.js
@@ -1,2 +1,3 @@
-document.write("ostatnia modyfikacja strony".fontcolor("red").bold().fontsize(7)+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("maroon").bold().fontsize(7)+"<br>"); 
+
 document.write(document.lastModified); 
